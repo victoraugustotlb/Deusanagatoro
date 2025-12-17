0fa4c366588785ba05760a51b1821894e5ec532b
Felipe Grolla Freitas <fegrolla0210@gmail.com>
2025-12-17 16:24:41 -0300

    feat

--- js/app.js
+++ js/app.js
@@ -219,6 +219,9 @@ window.App = () => {
 
                             {/* Filtro por Nome */}
                             <div className="filter-group">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+                                </svg>
                                 <label className="filter-label">Filtrar por Autor</label>
                                 {/* O CustomSelect deve se adaptar ao CSS global que alteramos */}
                                 <window.CustomSelect
@@ -228,8 +231,11 @@ window.App = () => {
                                 />
                             </div>
 
-                            {/* Ordenação Por data */}
+                            {/* Filtro de data */}
                             <div className="filter-group">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
+                                    <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
+                                </svg>
                                 <label className="filter-label">Filtro de data</label>
                                 <window.CustomSelect
                                     value={sortOrder}
