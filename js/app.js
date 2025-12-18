081ab964a6b88647366e46c32cc748c74fec4f65
Felipe Grolla Freitas <fegrolla0210@gmail.com>
2025-12-17 23:12:42 -0300

    style

--- js/app.js
+++ js/app.js
@@ -205,32 +205,31 @@ window.App = () => {
             </header>
             {/* ÁREA DE FILTROS */}
             <div className="filters-toolbar">
-              {/* Busca por Texto com Ícone */}
+              {/* Busca por Texto com Ícone no Header */}
               <div className="filter-group">
-                <label className="filter-label">Pesquisar</label>
-                <div className="input-icon-wrapper">
-                  {/* Ícone de Lupa SVG */}
+                <div className="filter-header">
                   <svg
-                    className="input-icon"
                     xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-search"
+                    viewBox="0 0 16 16"
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                    />
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                   </svg>
-
+                  <label className="filter-label">Pesquisar</label>
+                </div>
+                <div className="input-icon-wrapper">
                   <input
                     type="text"
                     className="styled-input"
                     placeholder="Título ou conteúdo..."
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
+                    style={{
+                      paddingLeft: "1rem",
+                    }} /* Remove padding extra já que o ícone subiu */
                   />
                 </div>
               </div>
