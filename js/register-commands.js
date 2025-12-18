6c2f3e18b97521077a6c7fac40dc01fceaf86ee5
Felipe Grolla Freitas <fegrolla0210@gmail.com>
2025-12-17 23:08:39 -0300

    otmz

--- js/register-commands.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-const { InstallGlobalCommands, ALL_COMMANDS } = require('./discord-utils.js');
-
-// Run the registration
-InstallGlobalCommands(process.env.APPLICATION_ID_BOT, ALL_COMMANDS);
